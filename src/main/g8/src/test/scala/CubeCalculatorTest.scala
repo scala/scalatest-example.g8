a1b4829fd0bea9e467861273398fd29e173c253d
Travis Lee <travis.sarles.lee@gmail.com>
2017-02-02 19:02:22 +0100

    changed test name

--- src/main/g8/src/test/scala/CubeCalculatorTest.scala
+++ src/main/g8/src/test/scala/CubeCalculatorTest.scala
@@ -1,8 +1,5 @@
-/**
-  * Created by travislee on 2/2/17.
-  */
 class CubeCalculatorTest extends org.scalatest.FunSuite {
-  test("cube of 3 is 27") {
+  test("CubeCalculator.cube") {
     assert(CubeCalculator.cube(3) === 27)
   }
 }
